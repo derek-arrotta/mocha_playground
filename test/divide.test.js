6dd30c0555f71b5c06627f2be9c0de836a4c6a3f
Derek Arrotta <derek.arrotta@gmail.com>
2021-01-04 14:07:36 -0500

    install chai, add test starter code
    install chai, add test starter code

--- test/divide.test.js
+++ test/divide.test.js
@@ -1,1 +1,12 @@
 const divide = require('../index');
+const expect = chai.expect;
+
+describe('Divide function', () => {
+  it('should divide positive integers correctly', () => {
+    // spec code here
+  });
+
+  it('should throw an error when divide by zero', () => {
+    // spec code here
+  });
+});
