1b1efb426fe8689a315c7302d6473b83950d3738
Derek Arrotta <derek.arrotta@gmail.com>
2021-01-04 14:43:27 -0500

    add my own test specs

--- test/divide.test.js
+++ test/divide.test.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect;
 const divide = require('../index');
 
 describe('Divide function', () => {
+  
   it('should divide positive integers correctly', () => {
     expect(divide(8, 4)).to.equal(2);
   });
@@ -9,4 +10,17 @@ describe('Divide function', () => {
   it('should throw an error when divide by zero', () => {
     expect(() => { divide(8, 0) }).to.throw();
   });
+
+  it('a & b should be a number, not a string', () => {
+    expect(() => divide('', '')).to.throw();
+  });
+
+  it('a & b should be a number, not a boolean', () => {
+    expect(() => divide(true, false)).to.throw();
+  });
+
+  it('a & b should be defined', () => {
+    expect(() => divide(undeclaredVariable1, undeclaredVariable2)).to.throw();
+  });
+
 });
