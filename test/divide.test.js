5e5e5b8d35f171b11f91ae376b82770c9e07ee00
Derek Arrotta <derek.arrotta@gmail.com>
2021-01-04 14:13:00 -0500

    add chai functionality & test spec code

--- test/divide.test.js
+++ test/divide.test.js
@@ -1,12 +1,34 @@
 const divide = require('../index');
-const expect = chai.expect;
+//const chai = require('chai');
+//const expect = chai.expect;
+//=
+const expect = require('chai').expect;
 
 describe('Divide function', () => {
   it('should divide positive integers correctly', () => {
     // spec code here
+      // define inputs
+      const a = 8;
+      const b = 4;
+      const expectedAnswer = 2;
+
+      // invoke the function
+      const actualAnswer = divide(a, b);
+
+      // assert that expected === actual
+      expect(actualAnswer).to.equal(expectedAnswer);
   });
 
   it('should throw an error when divide by zero', () => {
     // spec code here
+      // define inputs
+      const a = 8, b = 0;
+
+      // set up the function call
+      const fn = () => {
+        divide(a, b)
+      };
+
+      // assert that exception is thrown
+      expect(fn).to.throw();
   });
-});
