ee03f1945084f7753123bad5d6892788342b1e9d
Derek Arrotta <derek.arrotta@gmail.com>
2021-01-04 14:24:39 -0500

    condense code

--- test/divide.test.js
+++ test/divide.test.js
@@ -1,36 +1,12 @@
-const divide = require('../index');
-//const chai = require('chai');
-//const expect = chai.expect;
-//=
 const expect = require('chai').expect;
+const divide = require('../index');
 
 describe('Divide function', () => {
   it('should divide positive integers correctly', () => {
-    // spec code here
-      // define inputs
-      const a = 8;
-      const b = 4;
-      const expectedAnswer = 2;
-
-      // invoke the function
-      const actualAnswer = divide(a, b);
-
-      // assert that expected === actual
-      expect(actualAnswer).to.equal(expectedAnswer);
+    expect(divide(8, 4)).to.equal(2);
   });
 
   it('should throw an error when divide by zero', () => {
-    // spec code here
-      // define inputs
-      const a = 8, b = 0;
-
-      // set up the function call
-      const fn = () => {
-        divide(a, b)
-      };
-
-      // assert that exception is thrown
-      expect(fn).to.throw();
+    expect(() => { divide(8, 0) }).to.throw();
   });
-  
 });
