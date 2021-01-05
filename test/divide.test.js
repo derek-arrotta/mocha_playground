2c0f9a3cd6dc8d6f2e61a265401f4752094d8657
Derek Arrotta <derek.arrotta@gmail.com>
2021-01-05 16:37:27 -0500

    learning test files
    with chai assertion library

--- test/divide.test.js
+++ test/divide.test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const divide = require('../index');
 
 describe('Divide function', () => {
-  
+  /*
   it('should divide positive integers correctly', () => {
     expect(divide(8, 4)).to.equal(2);
   });
@@ -22,5 +22,27 @@ describe('Divide function', () => {
   it('a & b should be defined', () => {
     expect(() => divide(undeclaredVariable1, undeclaredVariable2)).to.throw();
   });
+  */
+
+  /*
+  it('should pass test1', () => {
+    expect(2).to.equal(2, '2 === 2'); // pass
+  })
+  it('should pass test2', () => {
+    expect(2).to.equal("2", '2 === "2"'); // fail, wrong types
+  })
+  it('should pass test3', () => {
+    expect(2).to.equal(3, '2 === 3'); // fail, wrong values
+  })
+  it('should pass test4', () => {
+    expect('hen').to.equal('Hen', "'hen' === 'Hen'"); // fail, string comparison is case sensitive
+  })
+  it('should pass test5', () => {
+    expect(true).to.equal(true, "true===true"); // 
+  })
+  it('should pass test6', () => {
+    expect(600/5).to.equal(600/10); // 
+  })
+  */
 
 });
---
+++ test/sort.test.js
@@ -0,0 +1,24 @@
+const expect = require("chai").expect;
+const sort = require("../sort");
+
+describe("sort function", () => {
+  /*
+
+  it("the list should be an array", () => {
+    expect(sort([1, 3, 2, 5, 4])).to.be.an('array');
+  });
+
+  it("the array values should be numbers", () => {
+    list = [1, 3, 2, 5, 4];
+    expect(list[1]).to.be.a('number');
+  });
+
+  it("the resulting array should be in ascending (i.e. 1,2,3) order", () => {
+    expect(sort([1, 3, 2, 5, 4])).to.deep.equal([1, 2, 3, 4, 5]);
+  });
+
+  it('empty array throws error', () => {
+    expect(() => { sort([]) } ).to.throw();
+  });
+  */
+});
